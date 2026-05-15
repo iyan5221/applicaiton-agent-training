20e002349734969da71b844c837c1bd8a1f9d7ac
Iyan Prasad <156579711+iyan5221@users.noreply.github.com>
2026-05-15 11:16:30 +0000

    v11 to v12

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -55,12 +55,11 @@ export function routerTransition() {
 export function slideToLeft() {
 	return trigger('routerTransition', [
 		transition(':enter', [
-			style({transform: 'translateX(100%)', position:'fixed', width:'100%'}),
-			animate('0.5s ease-in-out', style({transform: 'translateX(0%)'}))
+			style({transform: 'translateX(100%)', opacity: 0}),
+			animate('0.5s ease-in-out', style({transform: 'translateX(0%)', opacity: 1}))
 			]),
 		transition(':leave', [
-			style({transform: 'translateX(0%)', position:'fixed', width:'100%'}),
-			animate('0.5s ease-in-out', style({transform: 'translateX(-100%)'}))
+			animate('0.5s ease-in-out', style({transform: 'translateX(-100%)', opacity: 0}))
 			])
 		]);
 }
